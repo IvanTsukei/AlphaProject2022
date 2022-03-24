6ec84f8a09d6fac82879ae812b10a07847bed11d
Ivan P <74016834+IvanTsukei@users.noreply.github.com>
2022-03-24 08:07:46 -0400

    Fixing add proifle

--- src/backend/add_profile.py
+++ src/backend/add_profile.py
@@ -1,5 +1,5 @@
 import json
-
+from pathlib import Path
 
 
 profiles = {}
@@ -7,9 +7,15 @@ profiles = {}
 def add_profile():
     name = input()
     email = input()
-    profiles[name] = {'email': email}
+    tickers = [input()]
+    profiles[name] = {'email': email, 'ticker': tickers}
 
 add_profile()
 
-with open('profiles.json', 'w') as f:
-    json.dump(profiles, f)
+print(profiles)
+
+
+output_path = '/AlphaProject2022/src/backend'
+with open(output_path, 'a', encoding='utf8') as f:
+    json.dump(profiles, f, indent=4)
+f.close()

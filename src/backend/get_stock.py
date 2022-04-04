d57fdf5b92a8bd0f6c99cb4fc7605e92e8c12c4f
Ivan P <74016834+IvanTsukei@users.noreply.github.com>
2022-04-04 10:35:19 -0400

    Some new stock/portfolio functions

--- src/backend/get_stock.py
+++ src/backend/get_stock.py
@@ -1,10 +1,8 @@
 import pandas as pd
-from pandas_datareader import data as pdr 
-import matplotlib.pyplot as plt
+import datetime as dt
+import pandas_datareader.data as web
 import numpy as np
-import yfinance as yfin
-from pandas import Series, DataFrame
-from datetime import datetime
+from datetime import date
 
 ### Function Imports
 
@@ -14,39 +12,71 @@ from get_profile import get_profile
 
 ### Misc.
 
-yfin.pdr_override()
 df_m = pd.DataFrame()
 data = storage.read_data()
 
 ### Functions
 
-def current_stock_price(name, starting, ending):
+def stock_dividends(name, starting, ending):
     existing = data['profiles'][profile_index(name)]['stocks']
 
     if get_profile(name) == "Please enter a valid profile name.":
         print(get_profile(name))
     else:
         for stock in existing:
-            df_m[stock] = pdr.DataReader(stock, start= starting, end = ending, 
-                               data_source='yahoo', interval='1mo') ['Adj Close']
+            dividends = web.DataReader(stock, 'yahoo-dividends', starting, ending)
+            if len(dividends) == 0:
+                print(f'{stock} has no dividends for this time period.')
+            else:
+                print(dividends.head())
+                               
 
-def test(name, starting, ending):
-
-    data = storage.read_data()
-
+def stock_basic_history(name, starting, ending):
     existing = data['profiles'][profile_index(name)]['stocks']
 
     if get_profile(name) == "Please enter a valid profile name.":
         print(get_profile(name))
     else:
-        # stocks = " ".join(x for x in existing)
-        data = pdr.DataReader(existing, 'yahoo',starting,ending)
-        data
-
-current_stock_price('A', "2017-01-01", "2017-04-30")
-
-
-
+        for stock in existing:
+            df = web.DataReader(stock, 'yahoo', start = starting, end = ending)
+            if len(df) == 0:
+                print(f'{stock} has no history for this time period.')
+            else:
+                return df.head()
 
 
+### 
 
+def portfolio_extected_return(name, starting, ending):
+    existing = data['profiles'][profile_index(name)]['stocks']
+
+    if get_profile(name) == "Please enter a valid profile name.":
+        print(get_profile(name))
+    else:
+        priceData = web.DataReader(existing, 'yahoo', starting, ending)
+        returns = priceData['Adj Close'].pct_change()
+        weights = np.full((len(existing), 1), (1/len(existing)), dtype=float)
+        returns['Portfolio'] = np.dot(returns,weights)
+
+        portfolio_annual_return = returns['Portfolio'].mean()*250
+        print('Portfolio annual expected return is ' + str(np.round(portfolio_annual_return,2)*100)+'%')
+
+
+def portfolio_deviation(name, starting, ending):
+    existing = data['profiles'][profile_index(name)]['stocks']
+
+    if get_profile(name) == "Please enter a valid profile name.":
+        print(get_profile(name))
+    else:
+        for stock in existing:
+            df = web.DataReader(stock, 'yahoo', start = starting, end = ending)
+            if len(df) == 0:
+                print(f'{stock} has no history for this time period.')
+            else:
+                print(df.head())
+
+
+
+
+
+portfolio_extected_return('A', "2015-01-01", date.today())

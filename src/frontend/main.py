7fc77edbca00a6c2c4cce5e165c8348d130e0acc
Ivan P <74016834+IvanTsukei@users.noreply.github.com>
2022-04-17 22:47:09 -0400

    UI Updates

--- src/frontend/main.py
+++ src/frontend/main.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 import matplotlib
 
+from tkinter import *
+from tkinter import ttk
+
 #Widgets, to-do, different screens.
 from frontend.widgets.stock import StockWidget
 from frontend.widgets.profiles import ProfilesWidget
@@ -13,19 +16,22 @@ class App(tk.Tk):
 
     def __init__(self):
         super().__init__()
-        self.title("Cool Stock Project")
-        self.configure(bg='white')
-        w, h = self.winfo_screenwidth(), self.winfo_screenheight()
-        self.geometry("%dx%d+0+0" % (w, h))
+        self.title("Python Portfolio Tracker")
+        self.configure(bg='#1c1c1c')
+        # w, h = self.winfo_screenwidth(), self.winfo_screenheight()
+        # self.geometry("%dx%d+0+0" % (w, h))  # Fullscreen
+        self.geometry("900x900")
+        self.resizable(False, False) # Disables resizing
 
         self.plistWidget = ProfilesWidget(self, self.select_profile_callback)
-        self.plistWidget.grid(row = 0, column = 0, sticky = "ew") #show it.
+        self.plistWidget.grid(row = 0, column = 1) #show it.
+        self.grid_columnconfigure(0, minsize=300)
 
         self.pWidget = ProfileWidget(self, self.back_callback)
 
     def clear_screen(self):
-        self.plistWidget.grid_remove()
-        self.pWidget.grid_remove()
+        self.plistWidget.grid_forget()
+        self.pWidget.grid_forget()
 
     def back_callback(self):
         self.clear_screen()
@@ -38,9 +44,4 @@ class App(tk.Tk):
         self.pWidget.show(profile)
         self.pWidget.grid(row = 0, column = 0, sticky = "ew")
         self.state = "Profile"
-
-
-
-
-
 
--- src/frontend/widgets/profiles.py
+++ src/frontend/widgets/profiles.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from functools import partial
+from tkinter import *
+from tkinter import ttk
 
 from backend.list_profiles import return_profiles
 
@@ -9,14 +11,39 @@ class ProfilesWidget(tk.Frame):
         self.parent = parent
         self.profile_buttons = []
 
-        i = 0
+        ### Styling
+
+        style = ttk.Style()
+        style.theme_use("clam")
+        style.configure('TCombobox',
+                        fieldbackground="orange",
+                        
+                        foreground="black",
+                        darkcolor="#737373",
+                        selectbackground="#007fff",
+                        lightcolor="#737373",
+                        background="#737373"
+                        )
+
+        profileCombo = ttk.Combobox(state = "readonly", font="Verdana 12 bold") # Initiates the dropdown
+
+        ### Main
+
+        def callbackFunc(event):
+            profile = event.widget.get()
+            selected = select_profile_callback(profile)
+            return selected
+
         for profile in return_profiles():
-            button = tk.Button(self, text=profile['name'], command = partial(select_profile_callback, profile['name']))
-            button.grid(row = i, column = 0)
-            self.profile_buttons.append(button)
-            i += 1
+            profileCombo['values'] = tuple(list(profileCombo['values']) + [str(profile['name'])]) # Adds each profile to the dropdown list
 
+        ### Misc
+        
+        label = Label(text = 'Your Profiles', fg = 'white', bg = '#1c1c1c', font="Verdana 12")
+        label.grid(row = 1, column = 1)
 
-
+        profileCombo.set('Select a Profile') # Sets default text
+        profileCombo.grid(row = 2, column = 1)
+        profileCombo.bind("<<ComboboxSelected>>", callbackFunc)
 
 
